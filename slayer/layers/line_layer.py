f8c3c937754668a4d9da50c0c5960172106ab697
Andrew Duberstein <duberstein@uber.com>
2018-12-26 14:30:36 -0800

    Cut ASCII

--- slayer/layers/line_layer.py
+++ slayer/layers/line_layer.py
@@ -17,7 +17,7 @@ class LineLayer(Layer):
         target_position_field='target',
         **kwargs
     ):
-        super(Layer, self).__init__(data, **kwargs)
+        super(LineLayer, self).__init__(data, **kwargs)
         self.get_radius = make_js_get_radius(radius_field)
         self.get_source_position = make_js_get_source_position(source_position_field)
         self.get_target_position = make_js_get_target_position(target_position_field)
