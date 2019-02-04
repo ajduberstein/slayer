3da468b57f406703402b054dba03f690d0d52710
Andrew Duberstein <duberstein@uber.com>
2019-02-03 16:06:51 -0800

    Add time label to Style object

--- slayer/models/style.py
+++ slayer/models/style.py
@@ -20,14 +20,31 @@ TOOLTIP_DEFAULT = {
     'padding': '1vw',
 }
 
+DATETIME_LABEL_DEFAULT = {
+    'position': 'absolute',
+    'background': 'white',
+    'left': '10px',
+    'top': '10px',
+    'font-family': '"Helvetica Neue", sans-serif',
+    'font-size': '42px',
+    'color': 'black',
+    'margin': '10px',
+    'font-weight': 'bold',
+    'z-index': 2,
+    'padding': '10px'
+}
+
+
 class Style(object):
     def __init__(
         self,
         legend=LEGEND_DEFAULT,
-        tooltip=TOOLTIP_DEFAULT
+        tooltip=TOOLTIP_DEFAULT,
+        datetime_label=DATETIME_LABEL_DEFAULT
     ):
         self.legend = legend
         self.tooltip = tooltip
+        self.datetime_label = DATETIME_LABEL_DEFAULT
 
     def get_css(self, attr_name):
         obj_to_render = self.__dict__[attr_name]
