669d7c33d975f654be95690d6b70b61b72ae3edf
Andrew Duberstein <duberstein@uber.com>
2019-02-10 04:43:41 -0800

    Add tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -12,8 +12,15 @@ import pytest
 
 import subprocess
 
-def test_examples():
+def test_scatter_examples():
     subprocess.check_output(['python', './examples/scatterplot.py'])
     subprocess.check_output(['python', './examples/scatterplot_over_time.py'])
+    assert True
+
+def test_hexagon_examples():
     subprocess.check_output(['python', './examples/hexagon_layer.py'])
+    assert True
+
+def test_point_cloud_example():
+    subprocess.check_output(['python', './examples/point_cloud.py'])
     assert True
