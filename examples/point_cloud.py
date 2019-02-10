de66113eaa4a81091f03b6ea9a3dfa647f3f394b
Andrew Duberstein <duberstein@uber.com>
2019-02-10 04:41:29 -0800

    Add point cloud example

--- examples/point_cloud.py
+++ examples/point_cloud.py
@@ -1,25 +1,30 @@
-"""
-Example of a LIDAR point cloud, plotted using the PointCloudLayer.
+"""Example of a LIDAR point cloud, plotted using the PointCloudLayer.
+
+Plots over 1.6M points.
 """
 import slayer as sly
 import pandas as pd
 
 
-DATA_URL = 'https://raw.githubusercontent.com/ajduberstein/oakland_point_cloud/master/lidar_chunks_1.csv'
-DATA_URL_2 = 'https://raw.githubusercontent.com/ajduberstein/oakland_point_cloud/master/lidar_chunks_2.csv'
+URL = 'https://raw.githubusercontent.com/ajduberstein/oakland_point_cloud/master/%s'
+DATA_URL = URL % 'lidar_chunks_1.csv'
+DATA_URL_2 = URL % 'lidar_chunks_2.csv'
+LOOKUP_URL = URL % 'ground_truth_label.csv'
 lidar = pd.concat([pd.read_csv(DATA_URL), pd.read_csv(DATA_URL_2)])
+lookup = pd.read_csv(LOOKUP_URL)
+lidar = lidar.merge(lookup)
 
 v = sly.OrbitView()
 
 color_scale = sly.ColorScale(
     palette='random',
-    variable_name='label_id',
+    variable_name='label_name',
     scale_type='categorical_random')
 
-s = sly.Slayer(v) + \
+s = sly.Slayer(v, add_legend=False, blend=True) + \
     sly.PointCloudLayer(
-        lidar,
+        lidar[['x', 'y', 'z', 'label_name']],
         position=['x', 'y', 'z'],
         color=color_scale,
-        radius_pixels=1000)
+        radius_pixels=15)
 s.to_html('point_cloud.html', interactive=True)
